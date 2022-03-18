9c54c666b2a9f33113fac623371df867564a2ef8
Kociak1 <42005327+Kociak1@users.noreply.github.com>
2022-03-19 00:03:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-for($i=0;$i<20;$i++)
-echo "i wynosi $i<br>";
-echo "zgłoś się!!!";
-?>
+            $db = new PDO("pgsql:host=ec2-34-242-89-204.eu-west-1.compute.amazonaws.com;port=5432;dbname=ddqbqlr89jjlr1","hxezpxeiecwddf","2c587d88221ddd8209399210ac7e03465c4200655d4c395976559e5cede9f193");
+            $stmt = $db -> prepare('create table uczniowie(id int PRIMARY KEY AUTO_INCREMENT,imie text);');
+            $stmt -> execute();
+            $stmt = $db -> prepare('insert into uczniowie(imie) values("jan"),("adam"),("michał"),("piotr")');
+            $stmt -> execute();
+
+            $stmt = $db -> prepare('SELECT imie FROM uczniowie');
+            $stmt -> execute();
+
+            $result = $stmt -> fetchAll();
+            foreach($result as $a) {
+                ?>
+                <li><?=$a['imie']?></li>
+                <?php
+            }
+                $db=null;
+
+            ?>
+</body>
+</html>
