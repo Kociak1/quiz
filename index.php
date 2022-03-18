27a36ae10c6c5f6d570b26dff95ffe221e730289
Kociak1 <42005327+Kociak1@users.noreply.github.com>
2022-03-19 00:15:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     echo "works";
     try {
     $db = new PDO("pgsql:host=ec2-34-242-89-204.eu-west-1.compute.amazonaws.com;dbname=ddqbqlr89jjlr1","hxezpxeiecwddf","2c587d88221ddd8209399210ac7e03465c4200655d4c395976559e5cede9f193");
-            $stmt = $db -> prepare('create table uczniowie(id int PRIMARY KEY AUTO_INCREMENT,imie text);');
+            $stmt = $db -> prepare('create table uczniowie(id int PRIMARY KEY serial,imie text);');
             $stmt -> execute();
             $stmt = $db -> prepare('insert into uczniowie(imie) values("jan"),("adam"),("michał"),("piotr")');
             $stmt -> execute();
