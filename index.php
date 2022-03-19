cc0de0cc1b5bee8ccf50044799e93c597d74ff99
Kociak1 <42005327+Kociak1@users.noreply.github.com>
2022-03-19 13:58:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,7 @@
     echo "works";
     try {
     $db = new PDO("pgsql:host=ec2-34-242-89-204.eu-west-1.compute.amazonaws.com;dbname=ddqbqlr89jjlr1","hxezpxeiecwddf","2c587d88221ddd8209399210ac7e03465c4200655d4c395976559e5cede9f193");
-            $stmt = $db -> prepare('create table uczniowiee(id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,imie varchar(255));');
-            $stmt -> execute();
+            
             $stmt = $db -> query('insert into uczniowiee values (null,"jan")');
             
 
