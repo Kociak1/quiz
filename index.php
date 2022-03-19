d7cb1cff3d9151d4dc57f9b57194f4744c60f847
Kociak1 <42005327+Kociak1@users.noreply.github.com>
2022-03-19 13:59:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     try {
     $db = new PDO("pgsql:host=ec2-34-242-89-204.eu-west-1.compute.amazonaws.com;dbname=ddqbqlr89jjlr1","hxezpxeiecwddf","2c587d88221ddd8209399210ac7e03465c4200655d4c395976559e5cede9f193");
             
-            $stmt = $db -> query('insert into uczniowiee values (null,"jan")');
+            $stmt = $db -> query("insert into uczniowiee(imie) values ('jan')");
             
 
 
