89e76b619564597f4416cca3023cae473094112c
Kociak1 <42005327+Kociak1@users.noreply.github.com>
2022-03-20 14:09:27 +0100

    Update createstructure.php

--- createstructure.php
+++ createstructure.php
@@ -1,5 +1,5 @@
 <?php
 require_once "conn.php";
-$stmt = $db -> prepare('create table wyniki(id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,imie varchar(50), poprawne int(11), punkty float);');
+$stmt = $db -> prepare("create table wyniki(id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,imie varchar(50), poprawne int, punkty float);");
 $stmt -> execute();
 $db=null;
