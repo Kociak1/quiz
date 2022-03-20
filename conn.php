d2229dc34274e6c14172ce6ce6569ec64b549c37
Kociak1 <42005327+Kociak1@users.noreply.github.com>
2022-03-20 14:02:33 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 try {
    // $db = new PDO("mysql:host=localhost;dbname=quiz","root","");
-   $db = new PDO("pgsql:host=ec2-34-242-89-204.eu-west-1.compute.amazonaws.com;dbname=ddqbqlr89jjlr1","hxezpxeiecwddf","2c587d88221ddd8209399210ac7e03465c4200655d4c395976559e5cede9f193");
+   $db = new PDO("pgsql:host=ec2-52-31-217-108.eu-west-1.compute.amazonaws.com;dbname=dehi8it0p1vfdf","fqvopyaogyimnc","00693446c6da84b3991f6047992f75dde740e6215c1efff656ff0418cc124e74");
 } catch(Exception $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
